cb743eb87a70d3a96d85ec2a98abd511ae068bb1
Christian Tietze <christian.tietze@gmail.com>
2018-11-09 11:28:42 +0100

    fix hashValue crash

--- Lib/KeyHolder/RecordView.swift
+++ Lib/KeyHolder/RecordView.swift
@@ -264,12 +264,14 @@ public protocol RecordViewDelegate: class {
             let shiftTapped = inputModifiers.contains(.shift)
             let controlTapped = inputModifiers.contains(.control)
             let optionTapped = inputModifiers.contains(.option)
-            let totalHash = commandTapped.intValue + optionTapped.intValue + shiftTapped.intValue + controlTapped.intValue
-            if totalHash > 1 {
+            let modifiersCount = [commandTapped, optionTapped, shiftTapped, controlTapped]
+                .filter { $0 == true }
+                .count
+            if modifiersCount > 1 {
                 multiModifiers = true
                 return
             }
-            if multiModifiers || totalHash == 0 {
+            if multiModifiers || modifiersCount == 0 {
                 multiModifiers = false
                 return
             }
