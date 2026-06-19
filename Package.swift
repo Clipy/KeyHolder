25b639f2668aac3c9182264afb0254b09e8fb6c3
Econa77 <s.f.1992.ip@gmail.com>
2026-06-19 11:07:40 +0900

    Format package manifest dependencies

--- Package.swift
+++ Package.swift
@@ -13,12 +13,15 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Clipy/Magnet", .upToNextMinor(from: "3.5.0")),
-        .package(url: "https://github.com/Clipy/Sauce", .upToNextMinor(from: "2.5.0")),
+        .package(url: "https://github.com/Clipy/Sauce", .upToNextMinor(from: "2.5.0"))
     ],
     targets: [
         .target(
             name: "KeyHolder",
-            dependencies: ["Magnet", "Sauce"]
+            dependencies: [
+                "Magnet",
+                "Sauce"
+            ]
         ),
         .testTarget(
             name: "KeyHolderTests",
